0b631a6bd3b94daa1585b21b676a0371492cf300
bcid <bcid@uci.edu>
2019-05-13 22:55:11 -0700

    Pt 1

--- Parsetagram/AppDelegate.swift
+++ Parsetagram/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Parsetagram"
+                configuration.server = "https://pure-fortress-71570.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
